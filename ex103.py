399413242598f4e0e2631ade26726f9d52fc7f86
Andre Chaves <cchaves.andre@gmail.com>
2022-04-19 14:25:46 -0300

    exercícios

---
+++ ex103.py
@@ -0,0 +1,16 @@
+def ficha(jog='<desconhecido>', gol=0):
+    print(f'O jogador {jog} fez {gol} gol(s) no campeonato.')
+
+
+# Programa Principal
+print('-' * 20)
+n = str(input('Nome do Jogaor: '))
+g = str(input('Número de Gols: '))  # colocou como string pois aí ele deixa preencher vazio sem dar erro
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':  # nesse if eu passo só o parâmetro de gols se o nome for vazio - e no else eu passo os dois parâmetros
+    ficha(gol=g)
+else:
+    ficha(n, g)

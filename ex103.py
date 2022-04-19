1cbc039e4491ed458ebf80b5188441ed1ca825ac
Andre Chaves <cchaves.andre@gmail.com>
2022-04-19 14:26:36 -0300

    exercícios

--- ex103.py
+++ ex103.py
@@ -6,7 +6,7 @@ def ficha(jog='<desconhecido>', gol=0):
 print('-' * 20)
 n = str(input('Nome do Jogaor: '))
 g = str(input('Número de Gols: '))  # colocou como string pois aí ele deixa preencher vazio sem dar erro
-if g.isnumeric():
+if g.isnumeric():  # uso isso para realmente ver se o que foi digitado é um número
     g = int(g)
 else:
     g = 0
